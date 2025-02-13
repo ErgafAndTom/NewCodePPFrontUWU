c4befd77cad049abbf1fe51a1c4016424ed01d7b
AIATO <ErgafAndTom@gmail.com>
2025-02-13 05:34:13 +0200

    ddd

--- src/PrintPeaksFAinal/poslugi/newnomodals/note/Materials2NoteFront.jsx
+++ src/PrintPeaksFAinal/poslugi/newnomodals/note/Materials2NoteFront.jsx
@@ -235,16 +235,16 @@ const Materials2NoteFront = ({materialAndDrukFront, setMaterialAndDrukFront, cou
                         </select>
                     </div>
                 }
-                <LaminationNote
-                    materialAndDruk={materialAndDrukFront}
-                    setMaterialAndDruk={materialAndDrukFront}
-                    prices={prices}
-                    size={size}
-                    type={"Note"}
-                    buttonsArr={["З глянцевим ламінуванням",
-                        "З матовим ламінуванням",
-                        "З ламінуванням Soft Touch",]}
-                />
+                {/*<LaminationNote*/}
+                {/*    materialAndDruk={materialAndDrukFront}*/}
+                {/*    setMaterialAndDruk={materialAndDrukFront}*/}
+                {/*    prices={prices}*/}
+                {/*    size={size}*/}
+                {/*    type={"Note"}*/}
+                {/*    buttonsArr={["З глянцевим ламінуванням",*/}
+                {/*        "З матовим ламінуванням",*/}
+                {/*        "З ламінуванням Soft Touch",]}*/}
+                {/*/>*/}
 
                 {/*<div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: "2vw"}}>*/}
                 {/*    {buttonsArrDruk.map((item, index) => (*/}
